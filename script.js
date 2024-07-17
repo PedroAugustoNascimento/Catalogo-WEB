0f349cdb2506e8c2a593fcc3a9621099517559c6
Pedro Augusto Nascimento <nascimento.pedroaug@gmail.com>
2024-07-17 11:26:16 -0300

    comentários realizados

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-let time = 2000;
+let time = 2000; 
 let currentImg = 0;
 let img = document.querySelectorAll(".slider img");
 let max = img.length;
 
+/*Função para passar para a próxima imagem no banner*/
 function nextImg(){
     img[currentImg].classList.remove("selected");
     currentImg++;
@@ -13,10 +14,12 @@ function nextImg(){
     img[currentImg].classList.add("selected");
 }
 
+/*Função para chamar a nextImg() no intervalo da variável time*/
 function start(){
     setInterval(() => {
         nextImg();
     }, time);
 }
 
+/*Startando a função assim que a página é carregada*/
 window.addEventListener("load", start);
